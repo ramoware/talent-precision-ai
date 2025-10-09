bc370274e2f670cd6f42f990c88e69d16464a4af
ramoware <ramoware@users.noreply.github.com>
2025-10-09 17:44:28 +0530

    fixed the issue

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    appDir: false,
-  },
+  // Remove the experimental.appDir option entirely since it's deprecated
+  // The App Router is now stable and enabled by default in Next.js 13+
 }
 
 module.exports = nextConfig
