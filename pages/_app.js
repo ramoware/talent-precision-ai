cfd2a4a2b79df4355d85ec3158ba8cc2d3566125
ramoware <ramoware@users.noreply.github.com>
2025-10-09 17:49:35 +0530

    fixed the issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
+// pages/_app.js
+import { Inter } from 'next/font/google'
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const inter = Inter({ subsets: ['latin'] })
+
+export default function App({ Component, pageProps }) {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
